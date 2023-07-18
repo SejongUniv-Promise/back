08acc48df00718047aaa847243ab5e45298f2a3c
YOON <seoung59@gmail.com>
2023-07-18 14:26:08 +0900

    first commit

---
+++ src/main/java/rtsj/sejongPromise/SejongPromiseApplication.java
@@ -0,0 +1,13 @@
+package rtsj.sejongPromise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongPromiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejongPromiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rtsj/sejongPromise/SejongPromiseApplicationTests.java
@@ -0,0 +1,13 @@
+package rtsj.sejongPromise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongPromiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
